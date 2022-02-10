25d00ee3f2d9aca4d0667284ae1634aed0f6ab3d
Djordje Pejcic <djodjo1234@gmail.com>
2022-02-10 16:48:27 +0100

    Adding text of the following assignment || Homework || 07.02.2022.

--- src/d07_02_Tests/FormTest.java
+++ src/d07_02_Tests/FormTest.java
@@ -14,6 +14,23 @@ import d07_02_Pages.FormPage;
 
 public class FormTest {
 	
+//	1. Zadatak
+//	Skinite form.html stranicu (iz ovog foldera za domaci) na svom racunaru i otvorite je u pretrazivacu, iskopirajte url stranice
+//	Od klasa je potrebno:
+//	FormPage koja ima:
+//	getere za sve inpute
+//	geter za radio - //*[@name='gender'][@value='"+ radioValue +"'] - kao parametar prima value (male/female/middle) tog radio dugmeta
+//	geter za checkbox inpute - //*[@type='checkbox'][@value='"+ checkboxValue +"']  - kao parametar prima value (read_books, online_courses, opensource, tech_cons, tech_blogs, via_delivery - jednu od ovde navedenih) tog checkbox inputa
+//	getter koji hvata dugme treba da ceka da element postane klikabilan pre nego sto vrati element. Koristite waiter za ovaj slucaj.
+//	metodu koja vraca da li su podaci uspesno sacuvani, tako sto proverava da li element koji nosi poruku za atribut style ima vrednost "visibility: visible"
+//
+//	FormTest
+//	Ucitava stranicu
+//	Popunite formu proizvoljnim podacima
+//	Submitujte formu 
+//	Proverite da li su podaci uspesno sacuvani. (imate metodu iz page-a koja vam vraca da li je uspesno sacuvano, iskorisite je)
+//	U AfterClass metodi zatvaramo stranicu
+
 	private WebDriver driver;
 	private FormPage fp;
 
--- src/d07_02_Tests/KatalonTest.java
+++ src/d07_02_Tests/KatalonTest.java
@@ -16,6 +16,20 @@ import d07_02_Pages.Katalon_Wanted_Product_Page;
 
 public class KatalonTest {
 
+//	2. Zadatak
+//	Napisati test koji:
+//	Ucitava stranicu http://cms.demo.katalon.com/
+//	Vrsi klik na Shop link iz navigacije
+//	Otvara stranicu prvog proizvoda klikom na karticu prvog proizvoda
+//	Dodaje proizvod u korpu sa kolicinom 2
+//	Verifikuje poruku nakon dodavanja “has been added to your cart.”
+//	Odlazi u korpu klikom na dugme VIew Cart
+//	Verifikuje stanje u korpi, tj. da postoji proizvod sa kolicinom 2.
+//	Brise stavku iz korpe klikom na dugme x.
+//	Verifikuje poruku za praznu korpu.
+//	Osmislite pageve za ovaj zadatak!
+
+	
 	private WebDriver driver;
 	private Katalon_Homepage homePage;
 	private Katalon_Cart_Page cartPage;
