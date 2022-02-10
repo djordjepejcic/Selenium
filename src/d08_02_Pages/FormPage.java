19e5bdea0a06786001ac07000eec834b7f3c6726
Djordje Pejcic <djodjo1234@gmail.com>
2022-02-10 16:44:40 +0100

    Homework || 08.02.2022.

---
+++ src/d08_02_Pages/FormPage.java
@@ -0,0 +1,65 @@
+package d08_02_Pages;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class FormPage {
+
+	private WebDriver driver;
+
+	public FormPage(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	public WebElement getFullNameInput() {
+		return driver.findElement(By.id("first-name"));
+	}
+
+	public WebElement getDateOfBirth() {
+		return driver.findElement(By.id("dob"));
+	}
+
+	public WebElement getEmail() {
+		return driver.findElement(By.id("email"));
+	}
+
+	public WebElement getGender(String radioValue) {
+		return driver.findElement(By.xpath("//*[@name='gender'][@value='" + radioValue + "']"));
+	}
+
+	public Select getRole() {
+		Select select = new Select(driver.findElement(By.id("role")));
+		return select;
+	}
+
+	public WebElement getWaysOfDevelopment(String checkboxValue) {
+		return driver.findElement(By.xpath("//*[@type='checkbox'][@value='" + checkboxValue + "']"));
+	}
+
+	public WebElement getComment() {
+		return driver.findElement(By.id("comment"));
+	}
+
+	public WebElement getSubmitButton() {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.invisibilityOfElementWithText(By.id("submit"), "disabled"));
+
+		return driver.findElement(By.id("submit"));
+	}
+
+	public boolean isTheFormSavedSuccesfully() {
+		boolean saved = true;
+		List<WebElement> list = driver.findElements(By.xpath("//div[@style='visibility: visible']"));
+		if (list.size() == 0) {
+			saved = false;
+		}
+		return saved;
+	}
+}
---
+++ src/d08_02_Tests/FormTest.java
@@ -0,0 +1,94 @@
+package d08_02_Tests;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import d08_02_Pages.FormPage;
+
+public class FormTest {
+	private WebDriver driver;
+	private FormPage fp;
+
+	@BeforeMethod
+	public void beforeMethod() {
+		System.setProperty("webdriver.chrome.driver", "driver-lib\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+
+		fp = new FormPage(driver);
+		File file_1 = new File("files/form.html");
+		driver.manage().window().maximize();
+		driver.navigate().to(file_1.getAbsolutePath());
+	}
+
+	@Test
+	public void checkForm() throws IOException, InterruptedException {
+		File file_2 = new File("data/FormData.xlsx");
+		FileInputStream fis = new FileInputStream(file_2);
+		XSSFWorkbook wb = new XSSFWorkbook(fis);
+		SoftAssert sa = new SoftAssert();
+
+		XSSFSheet sheet = wb.getSheet("form");
+
+		for (int i = 1; i <= 6; i++) {
+
+			String fullName = sheet.getRow(i).getCell(0).getStringCellValue();
+			String gender = sheet.getRow(i).getCell(1).getStringCellValue();
+			Date dateOfBirth = sheet.getRow(i).getCell(2).getDateCellValue();
+			String email = sheet.getRow(i).getCell(3).getStringCellValue();
+			String role = sheet.getRow(i).getCell(4).getStringCellValue();
+			String waysOfDev = sheet.getRow(i).getCell(5).getStringCellValue();
+			String comment = sheet.getRow(i).getCell(6).getStringCellValue();
+
+			fp.getFullNameInput().sendKeys(fullName);
+			Thread.sleep(500);
+			fp.getGender(gender).click();
+			Thread.sleep(500);
+
+			DateFormat df = new SimpleDateFormat();
+			String dateOfBirth1 = df.format(dateOfBirth);
+
+			fp.getDateOfBirth().sendKeys(dateOfBirth1);
+			fp.getEmail().sendKeys(email);
+			Thread.sleep(500);
+			fp.getRole().selectByValue(role);
+			Thread.sleep(500);
+			fp.getWaysOfDevelopment(waysOfDev).click();
+			Thread.sleep(500);
+			fp.getComment().sendKeys(comment);
+			Thread.sleep(500);
+			fp.getSubmitButton().click();
+
+			sa.assertTrue(fp.isTheFormSavedSuccesfully(), "Form isn't saved properly!");
+			WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+			wait.until(ExpectedConditions.invisibilityOfElementWithText(
+					By.xpath("//*[@class='message-element poruka']"), "Uspesno ste sacuvali podatke!"));
+			driver.navigate().refresh();
+		}
+		sa.assertAll();
+	}
+
+	@AfterMethod
+	public void afterMethod() {
+		driver.quit();
+	}
+}
