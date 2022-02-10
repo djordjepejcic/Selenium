4ed9658ef1313d74c55779387e1dbbd449e46877
Djordje Pejcic <djodjo1234@gmail.com>
2022-02-10 16:46:54 +0100

    Adding text of the following assignment || Homework || 08.02.2022.

--- src/d08_02_Tests/FormTest.java
+++ src/d08_02_Tests/FormTest.java
@@ -24,6 +24,14 @@ import org.testng.asserts.SoftAssert;
 import d08_02_Pages.FormPage;
 
 public class FormTest {
+	
+//	1.Zadatak
+//	Za potrebe zadatka kreirati FormPage koji ima sve potrebne metode.
+//	Zatim kreirati FormTest koji 
+//	Ucitava form.html stranicu (form.html je u folderu za domaci skinite je i otvorite u chrome i iskopirajte url)
+//	I popunjava formu koristeci FormData.xlsx fajl (u folderu je za domaci)
+//	Postavite odgovarajuce waitere tako da se saceka sledeci unos podataka u formu nakon submitovanja
+	
 	private WebDriver driver;
 	private FormPage fp;
 
